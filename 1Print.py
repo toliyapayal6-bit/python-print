818ea6957235ba940b38d2e9528a5e8ecf82757a
toliyapayal6-bit <toliyapayal6@gmail.com>
2026-06-04 11:57:44 +0530

    print python

---
+++ 1Print.py
@@ -0,0 +1,115 @@
+#Write a Python program to print "Hello Python".
+print("Hello Python")
+
+# Name
+print("Toliya Payal")
+
+# college name
+print("Silver Oak ")
+
+#Welcome to Programming
+print("Welcome to programming")
+
+#numbers from 1 to 5
+print(1,2,3,4,5)
+
+#I love Python
+print("I Love to Python")
+
+#today’s day
+print("Thurday")
+
+# print your favorite subject
+print("Python")
+
+# print Learning Python is easy
+print("Lerning python is easy")
+
+# print Your city name
+print("Rajkot")
+
+# Create a variable to store your name and print it.
+Name = "Payal Toliya"
+print(Name)
+
+# Create a variable to store your age and print it
+Age = 22
+print(Age)
+
+# Create a variable to store your city name and print it
+City = "Rajkot"
+print(City)
+
+# Create a variable to store your school or college name and print it.
+
+College = "Silvdr Oak University"
+print(College)
+
+# Create a variable to store your favorite color and print it
+
+color = "pink"
+print(color)
+
+#Create a variable to store your favorite subject and print it
+
+subject = "Python"
+print(subject)
+
+#Create a variable to store your phone number and print it
+
+numder = (7016833840)
+print(numder)
+
+#Create a variable to store your email address and print it.
+email = ("toliyapayal6@email.com")
+print(email)
+
+#Create a variable to store your country name and print it
+country = ("India")
+
+#Create a variable to store a welcome message and print it.
+
+message = ("Welcom message")
+print(message)
+
+print("\n")
+# Write a Python program to take your name as input and print i
+
+Name =  input("My Name is")
+
+# Write a Python program to take your age as input and print it
+
+Age = int(input("Your age " ))
+
+#Write a Python program to take your city name as input and print it.
+
+City = input("city name ")
+
+#Write a Python program to take your school or college name as input and print it
+
+College = input("College name :")
+
+#Write a Python program to take your favorite color as input and print it
+
+color = input("your fevrite color :")
+
+#Write a Python program to take your favorite subject as input and print it.
+
+sub = input("your favrite subject :")
+
+# Write a Python program to take your phone number as input and print it
+
+numder = int(input("Enter your number :"))
+
+#Write a Python program to take your email address as input and print it.
+
+email = input("Enter email id :") 
+
+#Write a Python program to take your country name as input and print it
+
+country = input("Enter Your Country name:")
+
+
+#Write a Python program to take a welcome message as input and print it.
+
+mes = input("message :")
